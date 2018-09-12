aafe497697950a3b5c04b8da591cae91a83e2f57
ok0755 <37565231+ok0755@users.noreply.github.com>
2018-09-12 15:02:30 +0800

    Add files via upload

---
+++ create_bom.py
@@ -0,0 +1,81 @@
+#coding=utf-8
+import xlrd
+import xlsxwriter
+import os
+import re
+import multiprocessing
+
+class Bom(object):
+    def __init__(self,model):
+        self.i=0
+        self.model=model
+        self.th=[]
+        self.rootdir=u'J:\\PIE Process Manual\\新工序文件(CMP)\\工序手冊\\{}\\'.format(model)
+        self.get_file(self.rootdir)
+
+    #返回excel文件对象
+    def get_file(self,rootdir):
+        files=os.listdir(rootdir)
+        for fil in files:
+            if os.path.splitext(fil)[1]!='.pdf':
+                self.th.append(fil)
+        return self.th
+
+    #返回excel文件名和sheet表对象
+def openxls(rootdir,fil):
+    txt=[]
+    results=[]
+    xls=xlrd.open_workbook(rootdir+fil)
+    xls_sheet_names=xls.sheet_names()
+    for sheets in xls_sheet_names:
+        if not 'ECN' in sheets:
+            sht=xls.sheet_by_name(sheets)
+            rows=sht.nrows
+            for i in range(0,rows):
+                if sht.cell(i,9).ctype==1:
+                    txt.append(sht.cell(i,9).value)
+
+    txt=''.join(txt).replace(' ','').replace('\n','')
+    return fil,txt
+
+def writeExcel(dic,enter):
+    f=xlsxwriter.Workbook('{}.xlsx'.format(enter))
+    sheet=f.add_worksheet('sh')
+    i=0
+    for d,v in dic.items():
+        sheet.write(i,0,d)
+        sheet.write(i,1,v)
+        i+=1
+    sheet.set_column('A:A',30)
+    sheet.set_column('B:B',30)
+    f.close()
+
+if __name__=='__main__':
+    multiprocessing.freeze_support()
+    re_=re.compile('[A-Z]{3}-[0-9]{3}-[0-9]{3}',re.S)
+    v=[]
+    i=0
+    enter=raw_input(u'Input model,like as:"UM","UK":>>>')
+    rootdir=u'J:\\PIE Process Manual\\新工序文件(CMP)\\工序手冊\\{}\\'.format(enter)
+    f=Bom(enter)
+    t=f.th
+    results=[]
+    p=multiprocessing.Pool(processes=4)
+    for tt in t:
+        result=p.apply_async(openxls,(rootdir,tt,))
+        results.append(result)
+    p.close()
+    p.join()
+    dic={}
+    for x in results:
+        try:
+            txtvalue=x.get()
+            #print txtvalue[0].split('.')[0]
+            lists=re.findall(re_,txtvalue[1])
+            lists.sort()
+            key=' \n'.join(lists)
+            dic[txtvalue[0].split('.')[0]]=key
+        except:
+            pass
+    writeExcel(dic,enter)
+    print u'任务完成,{}{}.xlsx 已建立...\n Copyright by Zhoub'.format(os.getcwd(),enter)
